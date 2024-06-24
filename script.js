de386608d778238d1575537b42abb7bc035874cc
SuziHarima <152184174+SuziHarima@users.noreply.github.com>
2024-06-24 20:05:39 -0300

    adiciona respota para médias

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ function calcularMedia (notas) {
     return media;
 }
 
+let mediaFunction = prompt("Digite uma nota")
+
+    if (mediaFunction > 7) {
+        document.write("Parabéns vocês passou na média!")
+    } else {
+        document.write("Infelizmente você está de recuperação.")
+    }
+    
+
+
+// Crie uma função a qual recebe um valor numérico como parâmetro que irá ser a média; 
+// caso a média seja maior que 7, retorne em um document.write() a seguinte mensagem “Parabéns você passou na média !”, já, 
+// caso a média seja menor que 7, deve escrever em um document.write() a mensagem “Infelizmente você está de recuperação.”
