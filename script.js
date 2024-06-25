c196697b38b39c5ee73273834555a900afd476cc
SuziHarima <152184174+SuziHarima@users.noreply.github.com>
2024-06-24 21:41:10 -0300

    adiciona perguntas

--- script.js
+++ script.js
@@ -24,9 +24,38 @@ let studentName = ["Joaquina", "Kirara", "Salem", "Haley", "Luke"];
 
 studentName.forEach((Element) => {document.write("<br>" + Element)});
 
-// Utilizando estrutura de repetição for, realize a tabuada do número 8 e exibe em tela da seguinte forma
-
 let tabuada = 8;
 for (i=0; i<=10 ; i++){
     document.write("<br>" + "8 x " + i + " = " + (tabuada*i));
 };
+
+
+// Desenvolva um código JavaScript que faça o seguinte:
+
+// 1- Faça as seguintes 5 perguntas ao usuário:
+
+// Qual o nome do aluno?
+// Qual a idade do aluno?
+// Qual a série do aluno?
+// Qual o nome da escola?
+// Qual a sua matéria favorita?
+// 2- Utilize o método confirm() do JavaScript para perguntar ao usuário se ele confirma os dados inseridos
+//  (isso apenas ao final das 5 perguntas).
+
+// 4- Se o usuário confirmar, exiba todas as informações do aluno na tela utilizando o método document.write().
+
+// 5- Caso ele não aceite o programa, deve ser encerrado e nada deve ser feito. 
+// apenas mostre uma mensagem de que os dados não foram confirmado.
+
+
+let nome = prompt("Qual o nome do aluno?");
+let idade = prompt("Qual a idade do aluno?");
+let serie = prompt("Qual a série do aluno?");
+let escola = prompt("Qual o nome da escola?");
+let materiaFavorita = prompt("Qual a sua matéria favorita?")
+
+let confirmacao = window.confirm("Os dados estão certos?")
+
+if (confirmacao == true) {
+    document.write("<br>" + nome + "<br>" + idade + "<br>" + serie + "<br>" + escola + "<br>" + materiaFavorita)
+}
